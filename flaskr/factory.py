6803ffaf52b4a60c15365ade0ebfaf8f4ce5e217
26Claremont <WEIDC1@student.op.ac.nz>
2017-06-16 14:33:03 +1200

    Update factory.py

--- flaskr/factory.py
+++ flaskr/factory.py
@@ -1,6 +1,13 @@
 # all the imports
 import os
 import sqlite3
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
   app = Flask(__name__) # create the application instance :)
